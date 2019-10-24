8a8f8b2f53630ed07d5fbdff10c9eab8dcae45a7
GCChoi <ckc6842@gmail.com>
2019-10-24 20:07:32 +0900

    [Ch 11][GC] Change Timer.jsx to stateless function component

--- chapter11/gc/src/components/Timer.jsx
+++ chapter11/gc/src/components/Timer.jsx
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default class Timer extends Component {
-  render () {
-    if (this.props.timeLeft === 0) {
-      document.getElementById('end-of-time').play()
-    }
-
-    if (this.props.timeLeft === null || this.props.timeLeft === 0) return <div />
-
-    return (
-      <h1>Time left: {this.props.timeLeft}</h1>
-    )
+const Timer = (props) => {
+  if (props.timeLeft === 0) {
+    document.getElementById('end-of-time').play()
   }
+
+  if (props.timeLeft === null || props.timeLeft === 0) return <div />
+
+  return (
+    <h1>Time left: {props.timeLeft}</h1>
+  )
 }
+
+Timer.propTypes = {
+  timeLeft: PropTypes.number
+}
+
+export default Timer
