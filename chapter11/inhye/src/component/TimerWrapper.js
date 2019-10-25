a3480761c4f0004f0d27d4f7190a4c8534286bf9
InhyeJeong <inhyejeong59@gmail.com>
2019-10-25 19:53:32 +0900

    [CH11][inhye] Add Pause & Restart Function

--- chapter11/inhye/src/component/TimerWrapper.js
+++ chapter11/inhye/src/component/TimerWrapper.js
@@ -10,6 +10,7 @@ class TimerWrapper extends Component {
       timer: null,
     }
     this.startTimer = this.startTimer.bind(this)
+    this.pauseTime = this.pauseTime.bind(this)
   }
 
   startTimer (timeLeft) {
@@ -23,13 +24,19 @@ class TimerWrapper extends Component {
     return this.setState({ timeLeft: timeLeft, timer: timer })
   }
 
+  renderChangeButton () {
+    if (this.state.timeLeft === null) return
+    return <ChangeButton timeLeft={this.state.timeLeft} pauseTime={this.pauseTime} startTimer={this.startTimer}/>
+  }
+  pauseTime () {
+    clearInterval(this.state.timer)
+  }
 	render() {
 		return (
       <div className="row-fluid">
         <h2>Timer~~</h2>
         <div>
-          {/* TODO: 최초 로딩에 hide */}
-          <ChangeButton timeLeft={this.state.timeLeft}/>
+          {this.renderChangeButton()}
         </div>
         <div className="btn-group" role="group">
           <Button time="5" startTimer={this.startTimer} />
