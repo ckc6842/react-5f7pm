7a265371ee0a1a69cc03b9fbaac1ef346d339037
InhyeJeong <inhyejeong59@gmail.com>
2019-10-28 19:17:25 +0900

    [Ch 11][inhye] remove unused code

--- chapter11/inhye/src/component/TimerSlider.js
+++ chapter11/inhye/src/component/TimerSlider.js
@@ -5,17 +5,28 @@ class TimerSlider extends Component {
 	constructor (props) {
     super(props)
     this.state = {
+      maxValue: 0,
     }
+    this.onChangeSlider = this.onChangeSlider.bind(this)
+  }
+  onChangeSlider (e) {
+    console.log(e.target.value)
+  }
+  componentDidUpdate(prevProps) {
+    // if (prevProps) {
+    //   this.setState({maxValue: this.props.timeLeft})
+    // }
   }
 	render() {
+    if (this.props.timeLeft === null || this.props.timeLeft === 0) return <div />
 		return (
       <div>
         <input id="input-range"
                type="range"
                min="0"
-               max={this.props.timeLeft}
-               value={this.props.timeLeft ? this.props.timeLeft : 0}
-               disabled></input>
+               max={this.state.maxValue}
+               defaultValue={this.props.timeLeft ? this.props.timeLeft : 0}
+               onChange={this.onChangeSlider}></input>
       </div>
     )
 	}
