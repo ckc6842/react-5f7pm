09f39febfbcbd5b57f2bf172ba1b87b35ef5ed3e
InhyeJeong <inhyejeong59@gmail.com>
2020-01-13 20:02:17 +0900

    [CH 18][inhye] add code review comment

--- chapter18/inhye/src/store/containers/cart.js
+++ chapter18/inhye/src/store/containers/cart.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux'
 import { addToCart } from './../actions/cart'
-
+//  combinereducer 이후에는 이렇게 묶인다.
+//  state의 일부를 props로 보낸다.
 const mapStateToProps = (state) => {
   return {
     state: state.cartReducer.cart
   }
 }
-
+//  컴포넌트에서 사용하는 함수
+//  쓸만한 함수를 보낸다.
 const mapDispatchToProps = (dispatch, state) => {
   return {
     addCart: (item) => dispatch(addToCart(item))
