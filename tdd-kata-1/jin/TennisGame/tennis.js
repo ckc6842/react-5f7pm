b1bb05dfc9e76bdac5492f6298ba49818ebac6eb
Jinsang.Park <parkjs71@gmail.com>
2020-02-05 18:44:23 +0900

    [tdd-kata-1][jin] 게임 종료 후 점수 추가 요청시 에러 처리, 불필요한 코드 제거, 관련 테스트 추가

--- tdd-kata-1/jin/TennisGame/tennis.js
+++ tdd-kata-1/jin/TennisGame/tennis.js
@@ -1,18 +1,55 @@
-const GamePoints = require('./const')
+const { TENNIS_POINTS, GAME_STATUS } = require('./const')
 
 class TennisGame {
  
   constructor (leftPlayer, rightPlayer) {
     this.leftPlayer = leftPlayer
     this.rightPlayer = rightPlayer
+    this.advantagePlayer = null
+    this.gameStatus = GAME_STATUS.READY
+  }
+  
+  pointTo(player) {
+    // 게임이 이미 끝난상태라면 에러 처리
+    if (this.gameStatus === GAME_STATUS.END) {
+      throw(new Error('Game is over. point is ignored!'))
+    }
+
+    // 게임이 듀스 상태라면 어드벤티지 플레이어와 게임 상태를 업데이트
+    if (this.gameStatus === GAME_STATUS.DEUCE) {
+      this.updateDeuceAdvantageAndGameStatus(player)
+      return
+    }
+
+    // 스코어를 더한 점수가 게임 매치 포인트라면 듀스 상태인지 확인
+    let addedPoint = player.addPoint()
+    if (addedPoint === TENNIS_POINTS.MATCH) {
+      this.updateGameStatus()
+    }
+    else if (addedPoint > TENNIS_POINTS.MATCH) {
+      this.gameStatus = GAME_STATUS.END
+    }
   }
 
-  pointTo(player) {
-    if (player === this.leftPlayer) {
-      this.leftPlayer.addPoint()
+  updateGameStatus() {
+    if ((this.leftPlayer.getPoint() == TENNIS_POINTS.MATCH) &&
+        (this.rightPlayer.getPoint() == TENNIS_POINTS.MATCH)) {
+      this.gameStatus = GAME_STATUS.DEUCE;
     }
-    else if (player === this.rightPlayer) {
-      this.rightPlayer.addPoint()
+  }
+
+  updateDeuceAdvantageAndGameStatus(player) {
+    if (this.advantagePlayer == null) {
+      this.advantagePlayer = player
+      this.gameStatus = GAME_STATUS.DEUCE
+    }
+    else if (this.advantagePlayer == player) {
+      player.setPoint(TENNIS_POINTS.WIN)
+      this.gameStatus = GAME_STATUS.END
+    }
+    else {
+      this.advantagePlayer = null
+      this.gameStatus = GAME_STATUS.DEUCE
     }
   }
 
@@ -22,6 +59,26 @@ class TennisGame {
 
   getRightPlayer() {
     return this.rightPlayer
+  }
+
+  getAdvantagePlayer() {
+    return this.advantagePlayer
+  }
+
+  getGameStatus() {
+    return this.gameStatus
+  }
+
+  getWinner() {
+    if (this.gameStatus < GAME_STATUS.END) {
+      return undefined
+    }
+    if (this.leftPlayer.getPoint() > this.rightPlayer.getPoint()) {
+      return this.leftPlayer
+    } 
+    else {
+      return this.rightPlayer
+    }
   }
 
 }
@@ -41,16 +98,33 @@ class GamePlayer {
     return this.point
   }
 
+  setPoint(point) {
+    this.point = point
+  }
+
   addPoint() {
-    if (this.point === GamePoints.POINT_LOVE) {
-      this.point = GamePoints.POINT_FIFTEEN
+    if (this.point >= TENNIS_POINTS.WIN) {
+      return this.point
     }
-    else if (this.point < GamePoints.POINT_THIRTY) {
-      this.point = GamePoints.POINT_THIRTY
+
+    switch (this.point) {
+      case TENNIS_POINTS.LOVE:
+        this.point = TENNIS_POINTS.FIFTEEN
+        break
+      case TENNIS_POINTS.FIFTEEN:
+        this.point = TENNIS_POINTS.THIRTY
+        break
+      case TENNIS_POINTS.THIRTY:
+        this.point = TENNIS_POINTS.FORTY
+        break
+      case TENNIS_POINTS.FORTY:
+        this.point = TENNIS_POINTS.WIN
+        break
+      default:
+        this.point = TENNIS_POINTS.LOVE
     }
-    else if (this.point < GamePoints.POINT_FORTY) {
-      this.point = GamePoints.POINT_FORTY
-    }
+
+    return this.point
   }
 
 }
--- tdd-kata-1/jin/TennisGame/tennis.test.js
+++ tdd-kata-1/jin/TennisGame/tennis.test.js
@@ -1,7 +1,13 @@
+const { TENNIS_POINTS, GAME_STATUS } = require('./const')
 const { TennisGame, GamePlayer } = require('./tennis')
-const GamePoints = require('./const')
 
 let game, playerRed, playerBlue
+
+function takeGamePoint(player, count) {
+  for (let i=0; i < count; i++) {
+    game.pointTo(player)
+  }
+}
 
 describe('Test for basic playing', () => {
 
@@ -13,14 +19,73 @@ describe('Test for basic playing', () => {
 
   test('Red player taken first point', () => {
     game.pointTo(playerRed)
-    expect(game.getLeftPlayer().getPoint()).toBe(GamePoints.POINT_FIFTEEN)
+    expect(game.getLeftPlayer().getPoint()).toBe(TENNIS_POINTS.FIFTEEN)
   })
 
   test('Red player taken forty point', () => {
-    game.pointTo(playerRed)
-    game.pointTo(playerRed)
-    game.pointTo(playerRed)
-    expect(game.getLeftPlayer().getPoint()).toBe(GamePoints.POINT_FORTY)
+    takeGamePoint(playerRed, 3)
+    expect(game.getLeftPlayer().getPoint()).toBe(TENNIS_POINTS.FORTY)
+  })
+
+  test('Red player win perfectly', () => {
+    takeGamePoint(playerRed, 4)
+    expect(game.getWinner()).toBe(playerRed)
+  })
+
+  test('Blue player win perfectly', () => {
+    takeGamePoint(playerBlue, 4)
+    expect(game.getWinner()).toBe(playerBlue)
+  })
+
+  test('Blue player win by one point', () => {
+    takeGamePoint(playerBlue, 1)
+    takeGamePoint(playerRed, 2)
+    takeGamePoint(playerBlue, 3)
+    expect(game.getWinner()).toBe(playerBlue)
+  })
+
+})
+
+describe('Test for deuce playing', () => {
+
+  beforeEach(() => {
+    playerRed = new GamePlayer('Red')
+    playerBlue = new GamePlayer('Blue')
+    game = new TennisGame(playerRed, playerBlue)
+  })
+
+  test('Game status is deuce point', () => {
+    takeGamePoint(playerRed, 3)
+    takeGamePoint(playerBlue, 3)
+    expect(game.getGameStatus()).toBe(GAME_STATUS.DEUCE)
+  })
+
+  test('Red player taken advantage', () => {
+    takeGamePoint(playerRed, 3)
+    takeGamePoint(playerBlue, 3)
+    takeGamePoint(playerRed, 1)
+    expect(game.getAdvantagePlayer()).toBe(playerRed)
+  })
+
+  test('Blue player taken advantage', () => {
+    takeGamePoint(playerRed, 3)
+    takeGamePoint(playerBlue, 4)
+    expect(game.getAdvantagePlayer()).toBe(playerBlue)
+  })
+
+  test('Red player win finally after deuce', () => {
+    takeGamePoint(playerRed, 3)
+    takeGamePoint(playerBlue, 4)
+    takeGamePoint(playerRed, 1)
+    takeGamePoint(playerRed, 1)
+    takeGamePoint(playerRed, 1)
+    expect(game.getWinner()).toBe(playerRed)
+  })
+
+  test('Adding point after the game', () => {
+    takeGamePoint(playerRed, 3)
+    takeGamePoint(playerBlue, 5)
+    expect(() => takeGamePoint(playerBlue, 1)).toThrow()
   })
 
 })
