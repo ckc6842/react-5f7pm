61ccbf30df38e6fd67451f1435bfa939b94fca13
Jinsang.Park <parkjs71@gmail.com>
2020-02-06 13:22:48 +0900

    [tdd-kata-1][jin] TODO 관점으로 주석 추가, 프로젝트 설정 파일 추가

--- tdd-kata-1/jin/TennisGame/tennis.js
+++ tdd-kata-1/jin/TennisGame/tennis.js
@@ -1,7 +1,21 @@
 const { TENNIS_POINTS, GAME_STATUS } = require('./const')
 
+/**
+ * [TDD Kata] 테니스 게임 클래스
+ * 해당 테니스 게임은 두명의 플레이어가 1 세트의 경기만으로 이루어진다고 가정한다.
+ * 두 플레이어의 점수가 게임 매치 포인트에 해당하는 40점이 되면 듀스 상태를 제공한다.
+ * 듀스 상태에서는 두번 연속 포인트를 획득해야만 게임이 종료되고 승리할 수 있다.
+ */
 class TennisGame {
  
+  /**
+   * 테니스 게임 클래스 생성자는 명시적으로 게임을 할 플레이어 두명을 필수 인자로 받음
+   * TODO 생성자에서 필수 파라미터 검증을 통해 예외 처리
+   * game.start() 함수를 제공하고 해당 함수에서 게임을 시작할 수 있는 컨디션을 체크?
+   * 자바스크립트 클래스의 생성자 오류는 보통 어떻게 처리하는가?
+   * @param {GamePlayer} leftPlayer 
+   * @param {GamePlayer} rightPlayer 
+   */
   constructor (leftPlayer, rightPlayer) {
     this.leftPlayer = leftPlayer
     this.rightPlayer = rightPlayer
@@ -9,7 +23,15 @@ class TennisGame {
     this.gameStatus = GAME_STATUS.READY
   }
   
+  /**
+   * 파라미터로 받은 player 가 해당 함수 내에서 오염이 발생 (사이드 이펙트 발생 가능성)
+   * TODO 자바 스크립트에서 이걸 우아하게 처리할 수 있는 방법을 찾아서 수정할 것
+   * @param {GamePlayer} player 
+   */
   pointTo(player) {
+    if ((player instanceof GamePlayer) === false) {
+      throw(new Error('GamePlayer param is must required!'))
+    }
     // 게임이 이미 끝난상태라면 에러 처리
     if (this.gameStatus === GAME_STATUS.END) {
       throw(new Error('Game is over. point is ignored!'))
